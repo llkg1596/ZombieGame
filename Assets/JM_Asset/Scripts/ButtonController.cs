9165d3c92a8be91600e7dcae7bd391e13b912739
Katsee <ghtjd000129@naver.com>
2019-06-20 20:57:09 +0900

    모바일 플랫폼 변경

---
+++ Assets/JM_Asset/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    //public UnityEngine.UI.Image fade;
+    //float fades = 1.0f;
+    //float time = 0;
+
+    public void OnClickExit()
+    {
+
+        Application.Quit();
+        Debug.Log("End the Game");
+
+    }
+
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("Intro_Main_JM");
+    }
+
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("Intro_1_JM");
+    }
+}
---
+++ Assets/JM_Asset/Scripts/Intro_Main_PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_Main_PlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed = 5.0f;
+    public float rotateSpeed = 100.0f;
+
+    private Animator playerAnimator;
+    private Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Rotate();
+        playerAnimator.SetFloat("Move", 10.0f);
+    }
+   
+    private void Rotate()
+    {
+        float turn = rotateSpeed * Time.deltaTime;
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
+    }
+}
